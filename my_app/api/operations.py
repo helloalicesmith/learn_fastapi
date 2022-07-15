465831d40073b92aecaae06a8711342cbae06b97
helloalicesmith <helloalicesmith@gmail.com>
2022-07-15 18:39:00 +0300

    operations service was added

--- my_app/api/operations.py
+++ my_app/api/operations.py
@@ -1,20 +1,13 @@
 from fastapi import APIRouter
+from fastapi import Depends
 
-from .. import tables
 from ..models.operations import Operation
-from ..database import Session
+from ..services import operations
 
 router = APIRouter(
     prefix='/operations',
 )
 
 @router.get('/', response_model=list[Operation])
-def get_operations():
-    session = Session()
-    operations = (
-        session
-        .query(tables.Operation)
-        .all()
-    )
-
-    return operations
+def get_operations(service: operations.OperationsService = Depends()): # Depends -> говорит fastapi о внедрении зависимости
+    return service.get_list()
---
+++ my_app/services/operations.py
@@ -0,0 +1,17 @@
+from fastapi import Depends
+from sqlalchemy.orm import Session
+
+from ..database import get_session
+from .. import tables
+
+class OperationsService:
+    def __init__(self, session: Session = Depends(get_session)):
+        self.session = session
+
+    def get_list(self) -> list[tables.Operation]:
+        operations = (
+                self.session
+                .query(tables.Operation)
+                .all()
+        )
+        return operations
